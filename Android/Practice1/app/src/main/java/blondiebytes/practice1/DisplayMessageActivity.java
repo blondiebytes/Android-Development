df91945a6a97d0093d922250f16e8ecb39b65c30
Kathryn Hodge <kahodge@vassar.edu>
2015-04-20 19:40:30 -0400

    Soundboard App

---
+++ Android/Practice1/app/src/main/java/blondiebytes/practice1/DisplayMessageActivity.java
@@ -0,0 +1,50 @@
+
+package blondiebytes.practice1;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // get the intent that started the activity
+        Intent intent = getIntent();
+        // get the string that is bundled with the intent
+        String message = intent.getStringExtra(MainActivity.MESSAGE_KEY);
+
+        TextView tvMessage = new TextView(this);
+        tvMessage.setTextSize(40);
+        tvMessage.setText(message);
+        setContentView(tvMessage);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_display_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
