746cfcef8e0714037235eed039ddc25bd24c8278
Kathryn Hodge <kahodge@vassar.edu>
2015-04-22 23:04:55 -0400

    Adding a ScareATron App!

---
+++ Android/ScareATron/app/src/main/java/blondiebytes/scareatron/MainActivity.java
@@ -0,0 +1,78 @@
+package blondiebytes.scareatron;
+
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+// we need the implements because NewScareTacticFragment has an OnFragmentInteractionLister interface
+// which makes MainActivity have to have it.
+public class MainActivity extends ActionBarActivity implements NewScareTacticFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            // lets you handle fragment transactions
+            // pass information here
+            // R.id.container --> id of the activity's layout --> frame layout
+            // Take R.id.container's UI and add fragment's UI
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new NewScareTacticFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    // here we do refreshing of new scare tactics list
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
